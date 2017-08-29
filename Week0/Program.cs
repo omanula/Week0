83dcd542058543be7d27c846eb62891d2479f5ad
Olivia <31357493+omanula@users.noreply.github.com>
2017-08-29 14:56:28 -0600

    Week0 Homework

---
+++ Week0/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Week0
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week0/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Week0
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
